7af41d32bc75c2f7e6a1ee2887db52d62e1d9870
Arik Berenshtein <berenshtein@campus.technion.ac.il>
2022-10-12 21:45:33 +0300

    started learning "Tiled" and try to import a level

---
+++ Tiled/data/tsx/Objects.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Objects" tilewidth="2944" tileheight="1664" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="2944" height="1664" source="../../../graphics/tilesets/tiles.png"/>
+ </tile>
+</tileset>
